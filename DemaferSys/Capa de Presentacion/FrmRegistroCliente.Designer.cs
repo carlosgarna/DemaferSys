4cfd782e087654562efc27958a5e5c5b10c630a5
Carlos Garcia <carlos_fign@hotmail.com>
2018-06-28 00:54:08 -0500

    Software base
    Subir software base

---
+++ DemaferSys/Capa de Presentacion/FrmRegistroCliente.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace Capa_de_Presentacion
+{
+    partial class FrmRegistroCliente
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtNombres = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtApellidos = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDni = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtRuc = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtEmpresa = new System.Windows.Forms.TextBox();
+            this.rbnClienteNat = new System.Windows.Forms.RadioButton();
+            this.rbnClienteEmp = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(331, 106);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 27);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "&Salir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(331, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 27);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "&Grabar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(10, 362);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 16);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Teléfono";
+            // 
+            // txtNombres
+            // 
+            this.txtNombres.Location = new System.Drawing.Point(14, 279);
+            this.txtNombres.Name = "txtNombres";
+            this.txtNombres.Size = new System.Drawing.Size(298, 21);
+            this.txtNombres.TabIndex = 3;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(14, 380);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(298, 21);
+            this.txtTelefono.TabIndex = 3;
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(14, 329);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(298, 21);
+            this.txtDireccion.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(10, 311);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Dirección";
+            // 
+            // txtApellidos
+            // 
+            this.txtApellidos.Location = new System.Drawing.Point(14, 229);
+            this.txtApellidos.Name = "txtApellidos";
+            this.txtApellidos.Size = new System.Drawing.Size(298, 21);
+            this.txtApellidos.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(10, 211);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Apellidos";
+            // 
+            // txtDni
+            // 
+            this.txtDni.Location = new System.Drawing.Point(14, 66);
+            this.txtDni.MaxLength = 8;
+            this.txtDni.Name = "txtDni";
+            this.txtDni.Size = new System.Drawing.Size(298, 21);
+            this.txtDni.TabIndex = 3;
+            this.txtDni.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDni_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(10, 260);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombres";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(10, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "D.N.I";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(10, 98);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 16);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "R.U.C.";
+            // 
+            // txtRuc
+            // 
+            this.txtRuc.Location = new System.Drawing.Point(13, 118);
+            this.txtRuc.MaxLength = 11;
+            this.txtRuc.Name = "txtRuc";
+            this.txtRuc.Size = new System.Drawing.Size(299, 21);
+            this.txtRuc.TabIndex = 7;
+            this.txtRuc.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtRuc_KeyPress);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(11, 154);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(63, 16);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Empresa";
+            // 
+            // txtEmpresa
+            // 
+            this.txtEmpresa.Location = new System.Drawing.Point(12, 173);
+            this.txtEmpresa.Name = "txtEmpresa";
+            this.txtEmpresa.Size = new System.Drawing.Size(299, 21);
+            this.txtEmpresa.TabIndex = 9;
+            // 
+            // rbnClienteNat
+            // 
+            this.rbnClienteNat.AutoSize = true;
+            this.rbnClienteNat.Location = new System.Drawing.Point(14, 13);
+            this.rbnClienteNat.Name = "rbnClienteNat";
+            this.rbnClienteNat.Size = new System.Drawing.Size(113, 20);
+            this.rbnClienteNat.TabIndex = 10;
+            this.rbnClienteNat.Text = "Cliente Natural";
+            this.rbnClienteNat.UseVisualStyleBackColor = true;
+            this.rbnClienteNat.CheckedChanged += new System.EventHandler(this.rbnClienteNat_CheckedChanged);
+            // 
+            // rbnClienteEmp
+            // 
+            this.rbnClienteEmp.AutoSize = true;
+            this.rbnClienteEmp.Location = new System.Drawing.Point(187, 13);
+            this.rbnClienteEmp.Name = "rbnClienteEmp";
+            this.rbnClienteEmp.Size = new System.Drawing.Size(125, 20);
+            this.rbnClienteEmp.TabIndex = 11;
+            this.rbnClienteEmp.Text = "Cliente Empresa";
+            this.rbnClienteEmp.UseVisualStyleBackColor = true;
+            this.rbnClienteEmp.CheckedChanged += new System.EventHandler(this.rbnClienteEmp_CheckedChanged);
+            // 
+            // FrmRegistroCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 409);
+            this.Controls.Add(this.rbnClienteEmp);
+            this.Controls.Add(this.rbnClienteNat);
+            this.Controls.Add(this.txtEmpresa);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtRuc);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtNombres);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtDni);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtApellidos);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "FrmRegistroCliente";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmMantenimiento de Clientes";
+            this.Activated += new System.EventHandler(this.FrmRegistroCliente_Activated);
+            this.Load += new System.EventHandler(this.FrmRegistroCliente_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.TextBox txtNombres;
+        public System.Windows.Forms.TextBox txtTelefono;
+        public System.Windows.Forms.TextBox txtDireccion;
+        public System.Windows.Forms.TextBox txtApellidos;
+        public System.Windows.Forms.TextBox txtDni;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        public System.Windows.Forms.TextBox txtRuc;
+        public System.Windows.Forms.TextBox txtEmpresa;
+        public System.Windows.Forms.RadioButton rbnClienteNat;
+        public System.Windows.Forms.RadioButton rbnClienteEmp;
+    }
+}
+
---
+++ DemaferSys/Capa de Presentacion/FrmRegistroCliente.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using DevComponents.DotNetBar;
+using CapaLogicaNegocio;
+
+namespace Capa_de_Presentacion
+{
+    public partial class FrmRegistroCliente : DevComponents.DotNetBar.Metro.MetroForm
+    {
+        private clsCliente C = new clsCliente();
+
+        public FrmRegistroCliente()
+        {
+            InitializeComponent();
+            iniciarCamposBloqueados();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                        if (txtDireccion.Text.Trim() != "")
+                        {
+                            if (txtTelefono.Text.Trim() != "")
+                            {
+
+                                if (Program.Evento == 0)
+                                {
+                                    C.Ruc = txtRuc.Text;
+                                    C.Empresa = txtEmpresa.Text;
+                                    C.Dni = txtDni.Text;
+                                    C.Apellidos = txtApellidos.Text;
+                                    C.Nombres = txtNombres.Text;
+                                    C.Direccion = txtDireccion.Text;
+                                    C.Telefono = txtTelefono.Text;
+                                    DevComponents.DotNetBar.MessageBoxEx.Show(C.RegistrarCliente(), "Sistema de Ventas.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                    Limpiar();
+                                }
+                                else
+                                {
+                                    C.Ruc = txtRuc.Text;
+                                    C.Empresa = txtEmpresa.Text;
+                                    C.Dni = txtDni.Text;
+                                    C.Apellidos = txtApellidos.Text;
+                                    C.Nombres = txtNombres.Text;
+                                    C.Direccion = txtDireccion.Text;
+                                    C.Telefono = txtTelefono.Text;
+                                    DevComponents.DotNetBar.MessageBoxEx.Show(C.ActualizarCliente(), "Sistema de Ventas.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                    Limpiar();
+                                }
+                            }
+                            else
+                            {
+                                DevComponents.DotNetBar.MessageBoxEx.Show("Por Favor Ingrese N° de Teléfono o Celular.", "Sistema de Ventas.", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                                txtTelefono.Focus();
+                            }
+                        }
+                        else
+                        {
+                            DevComponents.DotNetBar.MessageBoxEx.Show("Por Favor Ingrese Dirección del Cliente.", "Sistema de Ventas.", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                            txtDireccion.Focus();
+                        }
+       }
+
+        private void Limpiar() {
+            txtDni.Clear();
+            txtRuc.Clear();
+            txtEmpresa.Clear();
+            txtApellidos.Clear();
+            txtNombres.Clear();
+            txtDireccion.Clear();
+            txtTelefono.Clear();
+            txtDni.Focus();
+        }
+
+        private void FrmRegistroCliente_Load(object sender, EventArgs e)
+        {
+            txtDni.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (DevComponents.DotNetBar.MessageBoxEx.Show("¿Está Seguro que Desea Salir.?", "Sistema de Ventas.", MessageBoxButtons.YesNo,MessageBoxIcon.Error) == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void FrmRegistroCliente_Activated(object sender, EventArgs e)
+        {
+            txtDni.Focus();
+        }
+
+        private void rbnClienteNat_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbnClienteNat.Checked == true)
+            {
+                txtDni.ReadOnly = false;
+                txtApellidos.ReadOnly = false;
+                txtNombres.ReadOnly = false;
+                txtDireccion.ReadOnly = false;
+                txtTelefono.ReadOnly = false;
+                txtRuc.ReadOnly = true;
+                txtEmpresa.ReadOnly = true;
+
+                txtDni.Text = "";
+                txtApellidos.Text = "";
+                txtNombres.Text = "";
+                txtDireccion.Text = "";
+                txtTelefono.Text = "";
+                txtRuc.Text = "";
+                txtEmpresa.Text = "";
+            }
+        }
+
+        private void rbnClienteEmp_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbnClienteEmp.Checked == true)
+            {
+                txtRuc.ReadOnly = false;
+                txtEmpresa.ReadOnly = false;
+                txtDireccion.ReadOnly = false;
+                txtTelefono.ReadOnly = false;
+                txtDni.ReadOnly = true;
+                txtApellidos.ReadOnly = true;
+                txtNombres.ReadOnly = true;    
+
+                txtDni.Text = "";
+                txtApellidos.Text = "";
+                txtNombres.Text = "";
+                txtDireccion.Text = "";
+                txtTelefono.Text = "";
+                txtRuc.Text = "";
+                txtEmpresa.Text = "";
+            }
+        }
+
+        private void txtDni_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsNumber(e.KeyChar)) && (e.KeyChar != (char)Keys.Back))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtRuc_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsNumber(e.KeyChar)) && (e.KeyChar != (char)Keys.Back))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void iniciarCamposBloqueados()
+        {
+            txtDni.ReadOnly = true;
+            txtRuc.ReadOnly = true;
+            txtEmpresa.ReadOnly = true;
+            txtApellidos.ReadOnly = true;
+            txtNombres.ReadOnly = true;
+            txtDireccion.ReadOnly = true;
+            txtTelefono.ReadOnly = true;
+        }
+    }
+}
